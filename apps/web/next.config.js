7286c006fad6f4f5dec390456df3b66c85e106bf
Stephen <survivingthequiet@gmail.com>
2023-03-18 20:30:28 +0000

    upgrade to next app experiemental

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,3 +1,12 @@
-module.exports = {
-  reactStrictMode: true,
+// module.exports = {
+//   reactStrictMode: true,
+// };
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
 };
+
+module.exports = nextConfig;
---
+++ apps/web/src/app/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "My Page Title",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <h1>My Page</h1>
+      <p>My page content</p>
+    </div>
+  );
+}
